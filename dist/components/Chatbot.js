c332176d8cf5a65cc683204b407bd06a6f4127f1
ismailkherbach <gi_kherbach@esi.dz>
2021-08-05 17:59:59 +0400

    update

--- dist/components/Chatbot.js
+++ dist/components/Chatbot.js
@@ -15,8 +15,6 @@ var _react = _interopRequireWildcard(require("react"));
 
 var _robotIcon = _interopRequireDefault(require("../assets/icons/robotIcon.svg"));
 
-var _clientIcon = _interopRequireDefault(require("../assets/icons/clientIcon.svg"));
-
 var _askIcon = _interopRequireDefault(require("../assets/icons/askIcon.svg"));
 
 var _closechat = _interopRequireDefault(require("../assets/icons/closechat.svg"));
@@ -27,20 +25,6 @@ var _sendmessage = _interopRequireDefault(require("../assets/icons/sendmessage.s
 
 var _askIconDisabled = _interopRequireDefault(require("../assets/icons/askIconDisabled.svg"));
 
-var _game = _interopRequireDefault(require("../assets/games/game1.svg"));
-
-var _game2 = _interopRequireDefault(require("../assets/games/game3.svg"));
-
-var _game3 = _interopRequireDefault(require("../assets/games/game5.svg"));
-
-var _game4 = _interopRequireDefault(require("../assets/games/game6.svg"));
-
-var _game5 = _interopRequireDefault(require("../assets/games/game7.svg"));
-
-var _game6 = _interopRequireDefault(require("../assets/games/game2.svg"));
-
-var _game7 = _interopRequireDefault(require("../assets/games/game4.svg"));
-
 var _axios = _interopRequireDefault(require("axios"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -50,8 +34,6 @@ function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "functio
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-var authUser = localStorage.getItem("user") ? localStorage.getItem("user") : null;
 
 class Chatbot extends _react.Component {
   constructor(props) {
@@ -128,13 +110,13 @@ class Chatbot extends _react.Component {
       question: "",
       questions: [],
       chat: false,
-      game: "",
+      game: "德州推广1",
       answers: []
     };
   }
 
   render() {
-    return /*#__PURE__*/_react.default.createElement("div", null, authUser && /*#__PURE__*/_react.default.createElement("div", {
+    return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("div", {
       className: "circle-chat-container flex fdr aic jcc"
     }, /*#__PURE__*/_react.default.createElement("div", {
       className: "lets-talk-chat flex fdr aic jcc"
@@ -149,7 +131,7 @@ class Chatbot extends _react.Component {
     }, /*#__PURE__*/_react.default.createElement("img", {
       alt: "img",
       src: _askIcon.default
-    }))), this.state.chat && authUser && /*#__PURE__*/_react.default.createElement("div", {
+    }))), this.state.chat && /*#__PURE__*/_react.default.createElement("div", {
       className: "chat-container flex fdc aic jcc"
     }, /*#__PURE__*/_react.default.createElement("div", {
       className: "chatbot"
@@ -178,64 +160,7 @@ class Chatbot extends _react.Component {
     }, /*#__PURE__*/_react.default.createElement("img", {
       alt: "img",
       src: _robotIcon.default
-    }), " ", /*#__PURE__*/_react.default.createElement("p", null, "\u60F3\u770B\u6211\u80FD\u505A\u4EC0\u4E48\u5417?")), /*#__PURE__*/_react.default.createElement("div", {
-      className: "games-container flex fdc aic jcc"
-    }, /*#__PURE__*/_react.default.createElement("p", null, "S\u9009\u62E9\u4F60\u60F3\u6D4B\u8BD5\u7684\u6E38\u620F"), /*#__PURE__*/_react.default.createElement("div", {
-      className: "games flex fdr aic jcc"
-    }, /*#__PURE__*/_react.default.createElement("div", {
-      className: "game ".concat(this.state.game === "HHpoker" ? "" : "", " flex fdc aic jcc"),
-      onClick: () => this.handleChooseGame("HHpoker")
-    }, /*#__PURE__*/_react.default.createElement("img", {
-      alt: "game1",
-      src: _game.default
-    })), /*#__PURE__*/_react.default.createElement("div", {
-      className: "game ".concat(this.state.game === "德州推广" ? "" : "", " flex fdc aic jcc"),
-      onClick: () => this.handleChooseGame("渔人码头")
-    }, /*#__PURE__*/_react.default.createElement("img", {
-      alt: "game1",
-      src: _game6.default
-    }))), /*#__PURE__*/_react.default.createElement("div", {
-      className: "games flex fdr aic jcc"
-    }, /*#__PURE__*/_react.default.createElement("div", {
-      className: "game ".concat(this.state.game === "德州推广" ? "" : "", " flex fdc aic jcc"),
-      onClick: () => this.handleChooseGame("聚合")
-    }, /*#__PURE__*/_react.default.createElement("img", {
-      alt: "game1",
-      src: _game2.default
-    })), /*#__PURE__*/_react.default.createElement("div", {
-      className: "game ".concat(this.state.game === "德州推广" ? "" : "", " flex fdc aic jcc"),
-      onClick: () => this.handleChooseGame("德州推广")
-    }, /*#__PURE__*/_react.default.createElement("img", {
-      alt: "game1",
-      src: _game7.default
-    }))), /*#__PURE__*/_react.default.createElement("div", {
-      className: "games flex fdr aic jcc"
-    }, /*#__PURE__*/_react.default.createElement("div", {
-      className: "game ".concat(this.state.game === "德州推广" ? "" : "", " flex fdc aic jcc"),
-      onClick: () => this.handleChooseGame("玩玩扑克")
-    }, /*#__PURE__*/_react.default.createElement("img", {
-      alt: "game1",
-      src: _game3.default
-    })), /*#__PURE__*/_react.default.createElement("div", {
-      className: "game ".concat(this.state.game === "德州推广" ? "" : "", " flex fdc aic jcc"),
-      onClick: () => this.handleChooseGame("德州推广1")
-    }, /*#__PURE__*/_react.default.createElement("img", {
-      alt: "game1",
-      src: _game4.default
-    }))), /*#__PURE__*/_react.default.createElement("div", {
-      className: "games flex fdr aic jcc"
-    }, /*#__PURE__*/_react.default.createElement("div", {
-      className: "game ".concat(this.state.game === "明星娱乐" ? "" : "", " flex fdc aic jcc"),
-      onClick: () => this.handleChooseGame("明星娱乐")
-    }, /*#__PURE__*/_react.default.createElement("img", {
-      alt: "game7",
-      src: _game5.default
-    })))), this.state.game !== "" && /*#__PURE__*/_react.default.createElement("div", {
-      className: "answer flex fdr aic jcfe"
-    }, /*#__PURE__*/_react.default.createElement("p", null, this.state.game), /*#__PURE__*/_react.default.createElement("img", {
-      alt: "img",
-      src: _clientIcon.default
-    })), this.state.questions.map((e, i) => {
+    }), " ", /*#__PURE__*/_react.default.createElement("p", null, "\u60F3\u770B\u6211\u80FD\u505A\u4EC0\u4E48\u5417?")), this.state.questions.map((e, i) => {
       return /*#__PURE__*/_react.default.createElement("div", {
         className: "answer flex fdc aife jcc"
       }, /*#__PURE__*/_react.default.createElement("p", null, e), this.state.answers.map((e, j) => {
